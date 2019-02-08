2280ebd67952d3677a558c5423d7858387a19186
Giuseppe Sensolini <46244893+JiuSenso@users.noreply.github.com>
2019-02-08 22:15:30 +0100

    new stuff

--- pid/test.c
+++ pid/test.c
@@ -1,84 +1,87 @@
 #include <stdlib.h>
-#include <unistd.h>
 #include <stdio.h>
-#include <stdint.h>
 
 #include "pid.h"
 
 int main(){
-
-    short ServoX = 0;
-    Ball* ball = (Ball*)malloc(sizeof(Ball));
-    ball->x[0] = 0;
-    ball->y[0] = 0;
-    ball->v = 0;
-    ball->phy = 0;
-   
 
     printf("\n# creating PID structs... ");
     PID_t* XPid = (PID_t*)malloc(sizeof(PID_t));
     PID_t* YPid = (PID_t*)malloc(sizeof(PID_t));
     printf("Done.\n");
 
-    //_ X PID SETUP__________________________________
-    printf("# Setting up XPid parameters... ");
+//_ X PID SETUP __________________________________
+    printf("\n# Setting up XPid parameters... ");
     setPid( XPid, SETPOINT_X, TOLLERANCE,
-            100 , // sample (in milliseconds)
-            0.0 , 0.0, 0.0 , // P, I, D
-            0.79 , 1.02 , 1.0); // kp. ki, kd
-    printf("Done.\n    Kp = %lf\n    Ki = %lf\n    Kd = %lf\n\n",
-            XPid->kp, XPid->ki, XPid->kd);
+            100, // dt (in milliseconds)
+            0.0 , 0.0, 0.0, // P, I, D
+            5.0 , 2.0 , 1.0, // kp. ki, kd
+            0.0, 0.0); // pre_error, error
+        printf("Done.\n    Kp = %lf\n    Kd = %lf\n    Ki = %lf\n",
+            XPid->kp, XPid->kd, XPid->ki);
+
+//_ Y PID SETUP __________________________________
+    printf("\n# Setting up YPid parameters... ");
+    setPid( YPid, SETPOINT_Y, TOLLERANCE,
+            0.1, // dt
+            0.0 , 0.0, 0.0, // P, I, D
+            5.0 , 2.0 , 1.0, // kp. ki, kd
+            0.0, 0.0); // pre_error, error
+        printf("Done.\n    Kp = %lf\n    Kd = %lf\n    Ki = %lf\n\n",
+            YPid->kp, YPid->kd, YPid->ki);
+//_______________________________________________
 
 
-    //:::::::::::::: L O O P :::::::::::::::::::::::::::::::::::::::::::::::::::::
-    int i = 0;
-    printf("\n# Compute(): done.\n    ServoX     = %d\n    x_ball_pos = %d",
-    ServoX, ball->x[0]);
+//_ X PID TEST __________________________________
+    struct Point_t ball_pos;
+    ball_pos.x = SETPOINT_X;
+    ball_pos.y = SETPOINT_Y;
     
-    //faccio un giro a vuoto
-    PIDCompute(XPid, ball->x[0], &ServoX);
-
-    while (i<12){
-
-        if (PIDCompute(XPid, ball->x[0], &ServoX)){
-            printf("\n# Compute(): done.\n    ServoX     = %d\n    x_ball_pos = %d",
-            ServoX, ball->x[0]);
-            printPidState(XPid);
-        }
-
-        else{
-            printf("# Compute(): none.\n    ServoX     = %d\n    x_ball_pos = %d",
-            ServoX, ball->x[0]);
-            printPidState(XPid);
-        }
-
-        ball->x[1] = ball->x[0];
-        ball->x[0] += 6; 
-
-        usleep(500000);
-        i++;
+    int t = 0;
+    for ( ; t<20 ; t++){ //ball not moving
+        printf("\n\n*** timestamp: %d ***", t);
+        PIDCompute(XPid, ball_pos.x);
+        printPID(XPid);
     }
 
+    for ( t=0 ; t<100 ; t++){
+        ball_pos.x += t;
+        printf("\n\n*** timestamp: %d ***\n    linear motion", t);
+        PIDCompute(XPid, ball_pos.x);
+        printPID(XPid);
+    }
 
-/*  //_ Y PID SETUP__________________________________
-    printf("# Setting up YPid parameters... ");
-    setPid( YPid, SETPOINT_Y, TOLLERANCE,
-            100 , // sample
-            0.0 , 0.0, 0.0 , // P, I, D
-            5.0 , 2.0 , 1.0); // kp. ki, kd
-    printf("Done.\n    Kp = %lf\n    Ki = %lf\n    Kd = %lf\n\n",
-            YPid->kp, YPid->ki, YPid->kid);
+    for ( t=0 ; t<100 ; t++){
+        ball_pos.x += -1.5*t;
+        printf("\n\n*** timestamp: %d ***\n    acceleration=1.5", t);
+        PIDCompute(XPid, ball_pos.x);
+        printPID(XPid);
+    }
 
-    short ServoY = 0;
-    x_ball_pos[0] = 260;
-    x_ball_pos[1] = 94;
+    /*
+    //testing random pattern movemen
+    for (t=0 ; t<2500 ; t+=10){
+        ball_pos.x += +1.5*t;
+        printf("\n\n*** timestamp: %d ***\n    random pattern", t);
+        PIDCompute(XPid, ball_pos.x);
+        printPID(XPid);
+    }
+     
+    TEST 1 FALED
+    *** to be fixed ***
+    1) ball over-react
+    2) use vectorizzation (AVX/SSE)
+    3) use pthread
+    4) Servo implementation
 
-*/
+
+    */
+//_________________________________________
 
 
 
 //__EXIT ROUTINE __________________________
-    printf("# Free PID struct... ");
+    printf("\n# Free PID struct... ");
     free(XPid);
     free(YPid);
     printf("Done.\n");
